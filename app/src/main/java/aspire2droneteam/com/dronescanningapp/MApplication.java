0814bac190b004123f8034d9d86c03497c97a4bb
Ilya Suglobov <il421@mail.ru>
2018-08-10 00:09:04 +1200

    10/08 Merged with Demo

--- app/src/main/java/aspire2droneteam/com/dronescanningapp/MApplication.java
+++ app/src/main/java/aspire2droneteam/com/dronescanningapp/MApplication.java
@@ -2,14 +2,22 @@ package aspire2droneteam.com.dronescanningapp;
 
 import android.app.Application;
 import android.content.Context;
-
 import com.secneo.sdk.Helper;
 
 public class MApplication extends Application {
-
+    private DemoApplication demoApplication;
     @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
+    protected void attachBaseContext(Context paramContext) {
+        super.attachBaseContext(paramContext);
         Helper.install(MApplication.this);
+        if (demoApplication == null) {
+            demoApplication = new DemoApplication();
+            demoApplication.setContext(this);
+        }
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        demoApplication.onCreate();
     }
 }
